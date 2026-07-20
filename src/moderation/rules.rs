8e5ba221148c86d4b10566e5a47c9b9322fe5277
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-20 15:22:58 -0300

    Create rules.rs

---
+++ src/moderation/rules.rs
@@ -0,0 +1,53 @@
+use serde::Deserialize;
+use std::fs;
+
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ModerationRules {
+
+    pub pornography: KeywordGroup,
+
+    pub gambling: KeywordGroup,
+
+    pub spam: KeywordGroup,
+
+    pub csam: KeywordGroup,
+
+    pub links: LinkGroup,
+}
+
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct KeywordGroup {
+    pub keywords: Vec<String>,
+}
+
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct LinkGroup {
+    pub domains: Vec<String>,
+}
+
+
+
+impl ModerationRules {
+
+
+    pub fn load(
+        path: &str
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+
+
+        let content =
+            fs::read_to_string(path)?;
+
+
+        let rules =
+            toml::from_str::<ModerationRules>(
+                &content
+            )?;
+
+
+        Ok(rules)
+    }
+}
