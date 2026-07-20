21515fc96b3b4ac5b0aa6200b6a6753caa43fbe9
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-20 15:23:11 -0300

    Update state.rs

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,6 +1,6 @@
-
 use crate::{
     core::config::Config,
+    moderation::rules::ModerationRules,
     storage::memory::MemoryStorage,
 };
 
@@ -11,20 +11,34 @@ pub struct AppState {
     pub config: Config,
 
     pub memory: MemoryStorage,
+
+    pub moderation: ModerationRules,
 }
+
 
 
 impl AppState {
 
 
     pub async fn new(
-        config: Config,
+        config: Config
     ) -> Result<Self, Box<dyn std::error::Error>> {
 
 
+        let moderation =
+            ModerationRules::load(
+                "config/moderation.toml"
+            )?;
+
+
         Ok(Self {
+
             config,
-            memory: MemoryStorage::new(),
+
+            memory:
+                MemoryStorage::new(),
+
+            moderation,
         })
     }
 }
